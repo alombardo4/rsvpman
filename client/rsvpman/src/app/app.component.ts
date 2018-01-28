9dd9c6ffd7ec5f6cac648f76a107997526febfd2
Alec Lombardo <alec.lombardo@gmail.com>
2018-01-28 10:17:20 -0500

    Add dynamic title

--- client/rsvpman/src/app/app.component.ts
+++ client/rsvpman/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/add/operator/filter';
+import { InfoService } from './services/info.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -18,12 +20,20 @@ export class AppComponent implements OnInit, OnDestroy {
 
   isAdmin = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private infoService: InfoService,
+              private titleService: Title) { }
 
   ngOnInit() {
     this.router$ = this.router.events.filter(ev => ev instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
       this.isAdmin = event.url.startsWith('/admin');
     })
+
+    this.infoService.getInfo()
+      .subscribe(info => {
+        const name = info.name;
+        this.titleService.setTitle(`${name} | RSVP`);
+      })
   }
 
   ngOnDestroy() {
